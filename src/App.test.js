4a7935901c94bac31372b8c6475d19370b069a2a
mukundamMayank <mayank.mukundam@students.iiit.ac.in>
2022-06-15 15:21:01 +0530

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import sum from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+//const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
